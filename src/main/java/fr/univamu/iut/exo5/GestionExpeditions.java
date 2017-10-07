83d3e00e94b26dbe91f60a34f59459bce9ea1180
Petru Valicov <petru.valicov@univ-amu.fr>
2017-10-07 14:35:37 +0200

    creation de la structure des paquets

---
+++ src/main/java/fr/univamu/iut/exo5/GestionExpeditions.java
@@ -0,0 +1,7 @@
+package fr.univamu.iut.exo5;
+
+public class GestionExpeditions {
+    public static void main(String[] args) {
+        throw new RuntimeException("Not yet implemented !");
+    }
+}
---
+++ src/main/java/fr/univamu/iut/exo6/GestionExpeditions.java
@@ -0,0 +1,7 @@
+package fr.univamu.iut.exo6;
+
+public class GestionExpeditions {
+    public static void main(String[] args) {
+        throw new RuntimeException("Not yet implemented !");
+    }
+}
