5ed4db19df7649ddbae06cec7074bc2be95ee81d
Petru Valicov <petru.valicov@univ-amu.fr>
2017-10-08 11:21:01 +0200

    Création des conteneurs

---
+++ src/main/java/fr/univamu/iut/Conteneur.java
@@ -0,0 +1,26 @@
+package fr.univamu.iut;
+
+import java.io.Serializable;
+
+public class Conteneur implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String expediteur;
+    private int poids;
+    private String destination;
+
+    public Conteneur(String expediteur, int poids, String destination) {
+        this.expediteur = expediteur;
+        this.poids = poids;
+        this.destination = destination;
+    }
+
+    @Override
+    public String toString() {
+        return "Conteneur expédié par la société " +
+                expediteur +
+                ", de poids " + poids +
+                " et à destination de " + destination;
+    }
+}
