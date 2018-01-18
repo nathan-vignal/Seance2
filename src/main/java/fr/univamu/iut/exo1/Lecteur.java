30d8f10882c39ad5557cf19522b9124a448fef1b
Petru Valicov <petru.valicov@univ-amu.fr>
2018-01-18 15:29:32 +0100

    Ajout de la classe Lecteur

---
+++ src/main/java/fr/univamu/iut/exo1/Lecteur.java
@@ -0,0 +1,12 @@
+package fr.univamu.iut.exo1;
+
+import fr.univamu.iut.Conteneur;
+
+import java.util.Collection;
+
+public class Lecteur {
+    public static Collection<Conteneur> charger(String fichier) {
+        // À compléter
+        return null;
+    }
+}
